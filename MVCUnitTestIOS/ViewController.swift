af70d913c52f3b23a7dc93d1e28c6672493568af
Timi Tejumola <tejumoladavid@gmail.com>
2019-11-16 04:12:37 +0100

    Initial Commit

---
+++ MVCUnitTestIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVCUnitTestIOS
+//
+//  Created by Timi Tejumola on 16/11/2019.
+//  Copyright © 2019 Timi Tejumola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVCUnitTestIOSTests/MVCUnitTestIOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVCUnitTestIOSTests.swift
+//  MVCUnitTestIOSTests
+//
+//  Created by Timi Tejumola on 16/11/2019.
+//  Copyright © 2019 Timi Tejumola. All rights reserved.
+//
+
+import XCTest
+@testable import MVCUnitTestIOS
+
+class MVCUnitTestIOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
